b92988da574413e6b3a016ce2218cec628ea8de1
Juan Cuzmar <jne6@hotmail.com>
2016-06-10 01:02:01 -0400

    (b)

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    user = require('./users.js'),
+    bodyParser = require('body-parser')
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.get('/users', user.findAll);
+app.get('/users/:id', user.findById);
+app.post('/users', user.addUser);
+app.put('/users/:id', user.updateUser);
+app.delete('/users/:id', user.deleteUser);
+
+app.listen(9000);
+console.log('Listening on port 9000...');
