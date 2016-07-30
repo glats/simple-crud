9005197007f0116537a0f53f72ee7a6cf8fd27df
Juan Cuzmar <jne6@hotmail.com>
2016-07-29 23:14:38 -0400

    Add config

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "DELETE, POST, PUT, GET");
+  next();
+});
 user.connect(function() {
    app.get('/users', user.findAll);
    app.get('/users/:id', user.findById);
